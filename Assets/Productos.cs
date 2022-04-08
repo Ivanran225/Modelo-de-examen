4d30be772306fe022cb914410bfdaa116699f921
Ivanran225 <ivanr.an225@gmail.com>
2022-04-08 11:30:00 -0300

    Initial commit

---
+++ Assets/Productos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Productos : MonoBehaviour
+{
+    public int p1;
+    public int p2;
+    public int p3;
+    public int dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int total = p1 + p2 + p3;
+        if (total > dinero) {
+            Debug.Log("No alcanza el dinero");
+        }
+        else
+        {
+            Debug.Log("El dinero alcanza");
+        }
+    }
+}
---
+++ Assets/dosnumeros.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dosnumeros : MonoBehaviour
+{
+    public string num1;
+    public string num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+}
